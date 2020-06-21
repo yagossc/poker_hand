ef0445e1d49d89715ca2cc7ae42eb861f29b1c9c
Yago Santos <yagossc@gmail.com>
2020-06-21 00:28:12 -0300

    Adapt analyzer's unit tests

--- cards/__test__/analyzer.spec.js
+++ cards/__test__/analyzer.spec.js
@@ -2,30 +2,22 @@ const analyzer = require('../analyzer');
 
 test("Test Poker Hands Classification", () => {
         let expectedHands = [
-            "straight-flush",
-            "four-of-a-kind",
-            "full-house",
-            "flush",
-            "straight",
-            "three-of-a-kind",
-            "two-pair",
-            "pair",
-            "high-card",
+            ["ah", "2h", "3h", "4h", "5h"],
+            ["2h", "2d", "2c", "3c", "3s"],
+            ["as", "2h", "3h", "4h", "5h"],
+            ["2h", "2d", "3d", "3c", "4s"],
+            ["2h", "9d", "3d", "5c", "4s"],
         ]
 
         let testHands = [
-            ["ah", "2h", "3h", "4h", "5h"],
-            ["2h", "2d", "2c", "2s", "3s"],
-            ["2h", "2d", "2c", "3c", "3s"],
-            ["7h", "2h", "3h", "4h", "5h"],
-            ["as", "2h", "3h", "4h", "5h"],
-            ["2h", "2d", "2c", "3c", "4s"],
-            ["2h", "2d", "3d", "3c", "4s"],
-            ["2h", "2d", "3d", "5c", "4s"],
-            ["2h", "9d", "3d", "5c", "4s"],
+            [["ah", "2h", "3h", "4h", "5h"], ["2h", "2d", "2c", "2s", "3s"]],
+            [["2h", "2d", "2c", "3c", "3s"], ["7h", "2h", "3h", "4h", "5h"]],
+            [["as", "2h", "3h", "4h", "5h"], ["2h", "2d", "2c", "3c", "4s"]],
+            [["2h", "2d", "3d", "3c", "4s"], ["2h", "2d", "3d", "5c", "4s"]],
+            [["2h", "9d", "3d", "5c", "4s"], ["2h", "7d", "3d", "5c", "4s"]],
         ];
 
-        testHands.forEach((hand, index) => {
-            expect(analyzer.classify(hand)).toBe(expectedHands[index]);
+        testHands.forEach((hands, index) => {
+            expect(analyzer.rankHands(hands[0], hands[1])).toEqual(expectedHands[index]);
         })
 })
